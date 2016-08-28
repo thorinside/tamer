ab815599297a684341f579326fee06a3dc234b10
Neal Sanche <neal.sanche@robotsandpencils.com>
2016-08-27 20:42:13 -0600

    Removing redundant file

--- init-user-db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE USER nealsanche;
-    CREATE DATABASE tamer;
-    GRANT ALL PRIVILEGES ON DATABASE tamer TO nealsanche;
-EOSQL
